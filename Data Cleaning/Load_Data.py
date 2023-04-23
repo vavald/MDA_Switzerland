1c614476222a415e0bbd4eb0182f8fb6a8229f96
Milko <milko.burkard1997@gmail.com>
2023-04-23 13:22:18 +0200

    Baseline Load Function start branch Milko Data Cleaning

--- Data Cleaning/Load_Data.py
+++ Data Cleaning/Load_Data.py
@@ -2,26 +2,40 @@ import os
 import pandas as pd
 
 
-def load_data(path_weather = "", path_noise = "", path_dir = os.path.dirname(__file__)):
+def load_data(  path_weather = "Raw Data/Weather Data",
+                path_noise = "Raw Data/Noise Data",
+                load_weather = True,
+                load_noise = True):
+    output = {}
+    #Rooth path of the file
+    path_dir = os.path.dirname(__file__)   
 
-    return(path_dir)
-    # csv_files_weather = [file for file in os.listdir(path_weather) if file.endswith('.csv')]
-    # data_weather = {}
-    # i = 1
-    # for file in csv_files_weather:
-    #     filepath = os.path.join(path_weather, file)
-    #     df = pd.read_csv(filepath)
-    #     key_name = "Q" + str(i)
-    #     i = i + 1
-    #     data_weather[key_name] = df
 
-    # csv_files_noise = [file for file in os.listdir(path_noise) if file.endswith('.csv')]
-    # data_noise = {}
-    # for file in csv_files_noise:
-    #     filepath = os.path.join(path_noise, file)
-    #     df = pd.read_csv(filepath, delimiter = ";")
-    #     data_noise[file] = df
-    
-    # return data_weather, data_noise
+    # Loading in the weather data
+    if load_weather:
+        path_weather = os.path.join(path_dir, path_weather)
+        csv_files_weather = [file for file in os.listdir(path_weather) if file.endswith('.csv')]
+        data_weather = {}
+        i = 1
+        for file in csv_files_weather:
+            filepath = os.path.join(path_weather, file)
+            df = pd.read_csv(filepath)
+            key_name = "Q" + str(i)
+            i = i + 1
+            data_weather[key_name] = df
+        output["weather"] = data_weather
 
-print(load_data())
+    # Loading in the noise data
+    if load_noise:
+        path_noise = os.path.join(path_dir, path_noise)
+        csv_files_noise = [file for file in os.listdir(path_noise) if file.endswith('.csv')]
+        data_noise = {}
+        for file in csv_files_noise:
+            filepath = os.path.join(path_noise, file)
+            df = pd.read_csv(filepath, delimiter = ";")
+            data_noise[file] = df
+        output["noise"] = data_noise
+        
+    return output
+
+data = load_data( load_weather = 0)
